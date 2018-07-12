5186f8e37f39e31258721edef86c28a3d9a1218e
scrappy1987 <jagordon87@msn.com>
2018-07-12 16:30:34 +0100

    inital commit

---
+++ src/main/java/com/qa/service/repository/AccountDBRepository.java
@@ -0,0 +1,82 @@
+package com.qa.service.repository;
+
+import static javax.transaction.Transactional.TxType.REQUIRED;
+import static javax.transaction.Transactional.TxType.SUPPORTS;
+
+import java.util.Collection;
+
+import javax.enterprise.inject.Default;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import javax.transaction.Transactional;
+
+import org.apache.log4j.Logger;
+
+import com.qa.domain.Account;
+
+import com.qa.util.JSONUtil;
+
+@Transactional(SUPPORTS)
+@Default
+public class AccountDBRepository  {
+
+	private static final Logger LOGGER = Logger.getLogger(AccountDBRepository.class);
+	
+	@PersistenceContext(unitName = "primary")
+	private EntityManager manager;
+
+	@Inject
+	private JSONUtil util;
+
+	public String getAllAccounts() {
+		LOGGER.info("In AccountDBRepository getAllAccounts");
+		Query query = manager.createQuery("Select a FROM Account a");
+		Collection<Account> accounts = (Collection<Account>) query.getResultList();
+		return util.getJSONForObject(accounts);
+	}
+
+	@Transactional(REQUIRED)
+	public String createAccount(String accout) {
+		LOGGER.info("In AccountDBRepository createAccount");
+		Account anAccount = util.getObjectForJSON(accout, Account.class);
+		manager.persist(anAccount);
+		return "{\"message\": \"account has been sucessfully added\"}";
+	}
+
+	@Transactional(REQUIRED)
+	public String updateAccount(Long id, String accountToUpdate) {
+		LOGGER.info("In AccountDBRepository updateAccount");
+		Account updatedAccount = util.getObjectForJSON(accountToUpdate, Account.class);
+		Account accountFromDB = findAccount(id);
+		if (accountToUpdate != null) {
+			accountFromDB = updatedAccount;
+			manager.merge(accountFromDB);
+		}
+		return "{\"message\": \"account sucessfully updated\"}";
+	}
+
+	@Transactional(REQUIRED)
+	public String deleteAccount(Long id) {
+		LOGGER.info("In AccountDBRepository deleteAccount");
+		Account accountInDB = findAccount(id);
+		if (accountInDB != null) {
+			manager.remove(accountInDB);
+		}
+		return "{\"message\": \"account sucessfully deleted\"}";
+	}
+
+	private Account findAccount(Long id) {
+		return manager.find(Account.class, id);
+	}
+
+	public void setManager(EntityManager manager) {
+		this.manager = manager;
+	}
+
+	public void setUtil(JSONUtil util) {
+		this.util = util;
+	}
+
+}
---
+++ src/main/resources/import.sql
@@ -0,0 +1,3 @@
+INSERT INTO ACCOUNT (id, firstname, secondname, accountnumber) VALUES (1, 'John', 'Doe', '123456')
+INSERT INTO ACCOUNT (id, firstname, secondname, accountnumber) VALUES (2, 'Jane', 'Doe', '123578')
+INSERT INTO ACCOUNT (id, firstname, secondname, accountnumber) VALUES (3, 'Jim', 'Taylor', '123690')
